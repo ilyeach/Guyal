ddf9b8044e428b3b5810258676fb487436bc66f3
ilyeach <133895939+ilyeach@users.noreply.github.com>
2023-10-27 03:02:50 +0530

    Add files via upload
    new

--- add.php
+++ add.php
@@ -1,9 +1,16 @@
 <?php
-session_start();
+include('db.php'); 
+	$object = new database();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['task'])) {
   $task = $_POST['task'];
+  $name=$_POST['username'];
+ 
 
+                                       
+ 
+  $sql="INSERT INTO `tasks`( `email_id`,`description`) VALUES ('$name','$task')";
+$result=mysqli_query($object->dbConnection(), $sql);
   if (!isset($_SESSION['tasks'])) {
     $_SESSION['tasks'] = [];
   }
